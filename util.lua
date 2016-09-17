0c5a2acd7a05c02db30839a7217414e74431b3eb
Travis Valenti <ennuuos@gmail.com>
2016-09-17 15:49:16 +1000

    Intersect
    - Added intersect function to util.lua

--- util.lua
+++ util.lua
@@ -1,7 +1,9 @@
 util = {
+  intersect = function(a, b)
+    return a.x + a.w > a.b and a.x < a.b + a.w and a.y + a.h > b.y and a.y < b.y + b.h
+  end,
 
 }
 
 
---TODO: Add intersect function
 --TODO: Add smallestIntersect function
