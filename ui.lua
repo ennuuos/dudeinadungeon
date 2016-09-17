efd0424b8254979b4ed9dd64bdc8afb164d163f2
Unknown <jeriah_russo@outlook.com>
2016-09-17 16:32:49 +1000

    UI Fixes
    - Fixed positioning of components
    - Fixed colouring of text

--- ui.lua
+++ ui.lua
@@ -5,9 +5,9 @@ function ui.drawStatusScreen()
   love.graphics.setColor(160, 107, 0)
   love.graphics.rectangle("fill", 0, 0, 250, 500)
   --TODO: Have player stats ready for use
-  love.graphics.print("Attack: " .. player.attack, 10, 10)
-  love.grpahics.setColor(0, 0, 0)
-  love.graphics.rectangle("fill", 25, 225, 200, 25)
+  love.graphics.setColor(0, 0, 0)
+  love.graphics.print("Attack: " .. 55, 10, 10)
+  love.graphics.rectangle("fill", 25, 470, 200, 25)
   love.graphics.setColor(44, 193, 36)
-  love.graphics.rectangle("fill", 25, 225, 100, 25)
+  love.graphics.rectangle("fill", 25, 470, 100, 25)
 end
